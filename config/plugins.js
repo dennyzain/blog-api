07464f19b380e2cb7bc2727f0b4f9b60b96f84de
dennyzain <abbasdenny24@gmail.com>
2022-04-04 10:50:54 +0700

    added certficates route

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,46 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        review: {
+          field: "slug",
+          references: "title",
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
+        project: {
+          field: "slug",
+          references: "title",
+        },
+        category: {
+          field: "slug",
+          references: "name",
         },
       },
     },
-
-  });
+  },
+  graphql: {
+    enabled: true,
+    config: {
+        defaultLimit: 10,
+        maxLimit: 20,
+        shadowCRUD: true,
+        apolloServer: {
+            tracing: true,
+        },
+    }
+},
+});
